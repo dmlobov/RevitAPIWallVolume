268615823522b156a1098a5a45bdc87643416889
D L <d.m.lobov@yandex.ru>
2023-01-31 17:37:08 +0300

    Добавьте файлы проекта.

---
+++ RevitAPIWallVolume/Main.cs
@@ -0,0 +1,37 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIWallVolume
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            IList<Reference> selectedElementRefList = uidoc.Selection.PickObjects(ObjectType.Element, new WallFilter(), "Выберете элементы по грани");
+            var wallList = new List<double>();
+            double sumVolume = 0;
+            foreach (var selectedElement in selectedElementRefList)
+            {
+                Wall owall = doc.GetElement(selectedElement) as Wall;
+                Parameter volume = owall.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED);
+                double volumeValue = UnitUtils.ConvertFromInternalUnits(volume.AsDouble(), DisplayUnitType.DUT_CUBIC_METERS);
+                sumVolume += volumeValue;
+            }
+            TaskDialog.Show("Объем стен", sumVolume.ToString());
+
+            return Result.Succeeded;
+        }
+    }
+}
